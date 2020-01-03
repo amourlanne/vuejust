fe3aeb52c209fcb8706e127c42a1752a6ac0e34b
Alexis Mourlanne <alexis.mourlanne@gmail.com>
2020-01-03 10:25:43 +0100

    fix packages

--- src/server-tsed/config/modules/security.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  authentication: {
-    secret: "5o}rCHCc)QX>9,9-|<&11&ZhE`p9uV@![ZP/9$z-/h!3OPbe4XQsQk&/&vXFpTV"
-  },
-  account: {
-    confirmation: "dxfsdfsdxf<sdxfsgsrjny ukj-(resdtrds"
-  }
-}
--- src/server-tsed/src/entity/PasswordResetToken.ts
+++
@@ -1,7 +0,0 @@
-import { Entity } from 'typeorm';
-import { AbstractToken } from './AbstractToken';
-
-@Entity('account_confirmation_tokens')
-export class AccountConfirmationToken extends AbstractToken{
-
-}
